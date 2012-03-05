ede536cdefa24eaefc34b78337e470046d8cc9b1
FireWhale <FireWu@Gmail.com>
2012-03-05 10:02:26 -0600

    another commitment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetapp::Application.config.secret_token = '5a18ce58d75580e1c61cb40d274b156b24dd850fcdc46bcfa5cd38123d3039304bed874e66c83a8706f295dd9620299a22038587cbd93f07e22c57767bfe73e6'
