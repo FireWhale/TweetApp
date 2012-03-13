497ed8f45ed46f188935ff30e1bcc9982c9cd5e7
FireWhale <FireWu@Gmail.com>
2012-03-12 21:57:01 -0500

    Couldn't get last section to work. Skipped chp 8 exercises and chp 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,17 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
-  # Return true if the user's password matches the submitted password.
-  def has_password?(submitted_password)
-    # Compare encrypted_password with the encrypted version of
-    # submitted_password.
-  end  
   
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil  if user.nil?
-    return user if user.has_password?(submitted_password)
-  end  
+    if user.nil?
+      nil
+    elsif user.has_password?(submitted_password)
+      user
+    else
+      nil
+    end
+  end
   
   private
 
